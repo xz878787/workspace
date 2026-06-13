69a945a62bf9a7055305e71175f372fd4edeb837
xz878787 <xiaozhi878787@qq.com>
2026-06-13 23:45:27 +0800

    Js多种数据类型笔记

--- fe/js/type/1.js
+++ fe/js/type/1.js
@@ -1,15 +1,21 @@
 //  表示空， 没有
 //null
-
+// primitive 原始 内存空间固定 ,  
 let a=null;
 let b=a;
 b=2;
-console.log(a);
+let obj1={name:"谢灵运"}
+let obj2=obj1; //
+console.log(obj1,obj2);
+
+obj2.company="快手";
+console.log(a,b);
 
 let obj={
     name:'张三',
     address:null,
 }
+console.log(obj.address);///null
 console.log(obj.age);//undefined
 
 let largeObject={
