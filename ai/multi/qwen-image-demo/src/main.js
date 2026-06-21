275a9a57cd1b9f311b1fdfb3f3140d03af9718fc
xz878787 <xiaozhi878787@qq.com>
2026-06-21 23:19:37 +0800

    feat:多模态+vite 学习

---
+++ ai/multi/qwen-image-demo/src/main.js
@@ -0,0 +1,56 @@
+// console.log('esm vite');
+const apiKey = import.meta.env.VITE_QWEN_API_KEY;
+const root = document.querySelector('#app');
+
+const generateImage = async () => {
+  const res = await fetch(
+    'https://dashscope.aliyuncs.com/api/v1/serv ices/aigc/multimodal-generation/generation',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+      },
+      //  请求体传输过程中 二进制字符
+      // JSON 字符串序列化
+      body: JSON.stringify({
+        "model": "qwen-image-2.0-pro",
+        "input": {
+          "messages": [
+            { "role": "user", "content": [
+              {
+                "image": "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250925/thtclx/input1.png"
+              },
+              {
+                "image": "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250925/iclsnx/input2.png"
+              },
+              {
+                "image": "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250925/gborgw/input3.png"
+              },
+              {
+                "text": "图1的女生穿着图2中的黑色裙子按图3的姿势坐下"
+              }
+            ]}
+          ]
+        },
+        "parameters": {
+          "n": 1,
+          "size": "1024*1536"
+        }
+      })
+    }
+  )
+  const data = await res.json();
+  console.log(data);
+  return data.output.choices[0].message.content[0].image;
+}
+
+const renderImage = (imageUrl) => {
+  root.innerHTML = `<img src="${imageUrl}" />`
+}
+
+const main = async () => {
+  const imageUrl = await generateImage();
+  renderImage(imageUrl);
+}
+main();
---
+++ ai/multi/readme.md
@@ -0,0 +1,20 @@
+# 多模态
+- 生图模型
+- 前端项目
+ axios/ fetch http 调用llm endpoint
+ apikey 写的明文 
+ - dotenv node 环境，  process 
+ - 前端环境 apikey. env 
+ ## 前端工程化 vite 
+ - 页面开发
+ - 工程开发 
+
+ ## vite
+ 项目脚手架
+ ## .env 流程
+ - vite  npm init vite
+ - .env.local VITE_QWEN_API_KEY
+ - import.meta.env.VITE_QWEN_API_KEY
+ 既可以使用llm,还可以保证key 不被泄露
+ VITE 就是前端项目在工程化这块的大管家
+ npm run dev VITE vite 接管整个项目
