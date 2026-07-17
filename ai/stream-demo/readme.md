9a758a30fa1f84f22384175a1abe6dbeb26e7b4c
xz878787 <xiaozhi878787@qq.com>
2026-07-17 23:24:39 +0800

    流式输出

---
+++ ai/stream-demo/readme.md
@@ -0,0 +1,15 @@
+# 流式输出
+
+- Agent 开发时代  
+  - Agent 越来越像人，走向AGI
+  - 如何将工作拆分， 将AI擅长的交给Agent ,我们审核， 不擅长的我们接管
+  - 项目工程初始化交给Agent 
+  1. 没有必要从0 开始写vue 项目工程
+  src/App.vue
+  index.html
+  2. 到GitHub 拉取一个模板项目 
+
+  - 热更新 hot reload
+  文件修改后，->刷新浏览器， ->丢失页面状态 -> 局部刷新
+
+  vue /react 数据状态 密密麻麻 
---
+++ ai/stream-demo/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+</script>
+
+<template>
+  <h1>Hello Vue 3!流式</h1>
+  <button @click="count++">点击我</button>
+  <div class="app">
+    <h1>Vue + Vite</h1>
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+.card {
+  padding: 2em;
+}
+</style>
