e5f7adcc5836f51633c8c7aafb7d7e96d2bec837
xz878787 <xiaozhi878787@qq.com>
2026-09-11 23:10:06 +0800

    Langgraph

--- ai/agent/Langgraph/langgraph-test/src/basic-graph.mjs
+++ ai/agent/Langgraph/langgraph-test/src/basic-graph.mjs
@@ -1,38 +1,43 @@
 import {
-  Annotation, // 注释 工作流的状态值的描述 数据 state
+  Annotation, // 注释 工作流的状态值的描述
   END, // 结束节点
-  START,  // 开始节点
-  StateGraph // 状态图 流程编排器 节点的组织
-} from '@langchain/langgraph';
-// Annotation 申明一个字段
+  START, // 开始节点
+  StateGraph, // 状态图 流程编排 节点的组织
+} from '@langchain/langgraph'
+
+// Annotation 声明一个字段
 // 数据部分
-const StateAnnotation  = Annotation.Root({
-  text: Annotation({ // 只有text 字段
-    // _prev 来到当前节点之前的状态， next 当前节点得到的状态
+const StateAnnotation = Annotation.Root({
+  text: Annotation({ // 只有text字段
+    // _prev 来到这个节点之前的上一个状态值
+    // next 当前节点得到的状态值
     // reducer 怎么处理状态的改变
-    reducer: (_prev, next) => next, // js 数组reduce 消消乐 text状态如何变
-    default: () => "", //默认值
+    reducer: (_prev, next) => next, // js数组reduce 消消乐 text 状态如何变
+    default: () => '', // 默认值
   })
 })
 // 定义节点
-// 函数就是节点 
-// 返回值 是下一个节点的状态
+// 函数就是节点
+// 返回值就是下一个节点的状态值
 // reducer 处理状态的改变
-const step1 = (state) => ({ text: `${state.text} -> step1`});
-const step2 = (state) => ({ text: `${state.text} -> step2`});
-// 实例化图工作流编排器
-const graph = new StateGraph(StateAnnotation)
-  // 申明所有节点 
-  .addNode("step1", step1)
-  .addNode("step2", step2)
-  .addEdge(START, "step1")
-  .addEdge("step1", "step2")
-  .addEdge("step2", END)
-  .compile() // 编译工作流 执行
+const step1 = (state) => ({ text: `${state.text} -> step1` })
+const step2 = (state) => ({ text: `${state.text} -> step2` })
+
+const graph = new StateGraph(StateAnnotation)// 实例化状态图
+  // 声明所有节点
+  .addNode('step1', step1)
+  .addNode('step2', step2)
+  .addEdge(START, 'step1')
+  .addEdge('step1', 'step2')
+  .addEdge('step2', END)
+  .compile() // 编译状态图 执行
 
 // 可视化的图
-// mermaid 文本画图工具， 简单的markdown, 自动生成流程图、
-// 可视化整个节点流转关系。 
-const drawable = await graph.getGraphAsync();
-const mermaid = drawable.drawMermaid({ withStyles: true });
-console.log(mermaid);
+// mermaid 文本画图工具，简单的markdown，自动生成流程图
+// 可视化整个节点流转关系
+const drawable = await graph.getGraphAsync()
+const mermaid = drawable.drawMermaid({ withStyle: true })
+console.log(mermaid)
+
+const result = await graph.invoke({ text: 'hello' })
+console.log(result)
